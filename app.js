4bfcf7407c189270f8124ed3b1e17d1ecfe3a249
Omar <omarelnagdy16@gmail.com>
2018-05-16 19:23:15 -0400

    color background

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 
 // Will look for a file in local directory called "views" and for a file with ".ejs" at the end
 app.set("view engine", "ejs");
-// app.use(bodyParser.urlencoded({extended: true})); // Needed for post requests ie: submitting a form
+app.use(bodyParser.urlencoded({extended: true})); // Needed for post requests ie: submitting a form
 app.use(express.static(__dirname + "/public")); // Use public folder to access css
 
 var connection = mysql.createConnection({
